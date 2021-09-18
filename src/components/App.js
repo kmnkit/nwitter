5006391cdcd8fc9d315a4dd826e77862ba864ee2
Marco Kang <kmnkit@gmail.com>
2021-09-18 20:12:15 +0900

    realtime nweet

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,15 @@ import { onAuthStateChanged } from "firebase/auth";
 function App() {
   const [init, setInit] = useState(false); // 유저 로그인 로드를 기다리기 위한 flag
   const [isLoggedIn, setIsLoggedIn] = useState(false); // 유저 로그인 여부 flag
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     onAuthStateChanged(authService, (user) => {
       if (user) {
         setIsLoggedIn(true); // 유저가 인증된 경우 로그인 flag를 true로 변경        
+        setUserObj(user);
       } else {
         setIsLoggedIn(false); // 유저가 인증되지 않은 경우 flag를 false로 유지
+        setUserObj(null);
       }
       setInit(true); // 유저 로그인 되면 set true
     });
@@ -19,7 +22,7 @@ function App() {
   return (
     <>
       {/* 유저 로그인이 확인 되면 isLoggedIn을 true로 하고 그 전까지는 문구만 출력 */}
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
       <footer>&copy; Nwitter-Marco {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,30 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
-import { collection, addDoc, getDocs, query, serverTimestamp } from "firebase/firestore";
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
 
-const Home = () => {
+const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    const getNweets = async () => {
-        const q = query(collection(dbService, "nweets"));
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-            const nweetObj = {
-                ...doc.data(),
-                id: doc.id,
-            }
-            setNweets(prev => [nweetObj, ...prev]);
-        });
-    };
     useEffect(() => {
-        getNweets();
+        const q = query(collection(dbService, "nweets"), orderBy("createdAt", "desc"));
+        onSnapshot(q, (querySnapshot) => {
+            const nweetArray = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setNweets(nweetArray);
+        });
     }, []);
     const onSubmit = async (e) => {
         e.preventDefault();
-        console.log(`서브밋 하는 느윗:${nweet}`);
         await addDoc(collection(dbService, "nweets"), {
-            nweet,
+            text: nweet,
             createdAt: serverTimestamp(),
+            creatorId: userObj.uid
         });
         setNweet("");
     };
@@ -47,7 +43,7 @@ const Home = () => {
                     nweets.map(nweet => (
                         <div key={nweet.id}>
                             <h4>
-                                {nweet.nweet}
+                                {nweet.text}
                             </h4>
                         </div>
                     ))
