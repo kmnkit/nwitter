2cd8eb35c1f158ea7f74df3466c4ea58e2b77bb9
Marco Kang <kmnkit@gmail.com>
2021-09-18 19:21:35 +0900

    create and get documents

--- src/fbase.js
+++ src/fbase.js
@@ -1,10 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig);
 export const authService = getAuth();
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { dbService } from 'fbase';
+import { collection, addDoc, getDocs, query, serverTimestamp } from "firebase/firestore";
+
 const Home = () => {
     const [nweet, setNweet] = useState("");
-    const onSubmit = (e) => {
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const q = query(collection(dbService, "nweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            const nweetObj = {
+                ...doc.data(),
+                id: doc.id,
+            }
+            setNweets(prev => [nweetObj, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
+    const onSubmit = async (e) => {
         e.preventDefault();
+        console.log(`서브밋 하는 느윗:${nweet}`);
+        await addDoc(collection(dbService, "nweets"), {
+            nweet,
+            createdAt: serverTimestamp(),
+        });
+        setNweet("");
     };
     const onChange = (e) => {
         const { target: { value } } = e;
         setNweet(value);
     };
     return (
-        <form>
-            <input
-                type="text"
-                value={nweet}
-                onChange={onChange}
-                placeholder="What's on your mind?" maxLength={120} />
-            <input type="submit" value="Nweet" />
-        </form>
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    value={nweet}
+                    onChange={onChange}
+                    placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+            <div>
+                {
+                    nweets.map(nweet => (
+                        <div key={nweet.id}>
+                            <h4>
+                                {nweet.nweet}
+                            </h4>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
     );
 };
 export default Home;
