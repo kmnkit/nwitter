7db1468a943ed5e96ba6e59f1c597c70e8e9a203
Marco Kang <kmnkit@gmail.com>
2021-09-18 16:07:00 +0900

    Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false); // 유저 로그인 로드를 기다리기 위한 flag
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 유저 로그인 여부 flag
+  useEffect(() => {
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        setIsLoggedIn(true); // 유저가 인증된 경우 로그인 flag를 true로 변경        
+      } else {
+        setIsLoggedIn(false); // 유저가 인증되지 않은 경우 flag를 false로 유지
+      }
+      setInit(true); // 유저 로그인 되면 set true
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {/* 유저 로그인이 확인 되면 isLoggedIn을 true로 하고 그 전까지는 문구만 출력 */}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; Nwitter-Marco {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -7,6 +7,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (e) => {
         const {
@@ -33,12 +34,10 @@ const Auth = () => {
             }
             console.log(data);
         } catch (error) {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode);
-            console.log(errorMessage);
+            setError(error.errorMessage);
         }
-    }
+    };
+    const toggleAccount = () => setNewAccount(prev => !prev);
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -59,7 +58,9 @@ const Auth = () => {
                     onChange={onChange}
                 />
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Log In" : "Create Account"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
