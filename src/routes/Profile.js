40ec07bae14e64e7cc808688a8959525c64dda8c
Marco Kang <kmnkit@gmail.com>
2021-09-19 16:33:04 +0900

    filter my nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,12 +1,28 @@
-import { authService } from 'fbase';
-import React from 'react';
+import { collection, onSnapshot, orderBy, query, where } from '@firebase/firestore';
+import { authService, dbService } from 'fbase';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const history = useHistory();
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
+    const getMyNweets = async () => {
+        const q = query(
+            collection(dbService, "nweets"),
+            where("creatorId", "==", userObj.uid),
+            orderBy("createdAt", "desc")
+        );
+        onSnapshot(q, (querySnapshot) => {
+            const nweetArray = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            console.log(nweetArray);
+        });
+    };
+    useEffect(() => { getMyNweets(); }, []);
     return (
         <>
             <button onClick={onLogOutClick}>Log Out</button>
